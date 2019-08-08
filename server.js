40a29105c4138723e86117b4e97792f2e22d11e4
kishan.si <kishan.si@media.net>
2019-08-08 19:24:50 +0530

    Checking Redis

--- server.js
+++ server.js
@@ -1,13 +1,56 @@
 const express = require('express');
+const redis = require('redis');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+const redisClient = redis.createClient();
 
-app.get('/:id', (req, res) => {
+app.use(bodyParser.json());
 
-    const id = req.params.id
-    res.status(200).send(id);
+app.get('/:key', (req, res) => {
+
+    const key = req.params.key
+
+    redisClient.get(key, (err, reply) => {
+
+        if(reply === null)
+        {    
+            res.status(404).send();
+        }
+        else
+        {
+            const msg = {
+                value: reply
+            }
+    
+            res.status(200).json(msg);
+        }
+        
+    })
 });
+
+
+app.post('/', (req, res) => {
+
+    const key = req.body.key;
+    const value = req.body.value;
+
+    redisClient.set(key, value, (err, rep) => {
+        const msg = {
+            reply: rep
+        }
+        res.status(200).json(msg);
+    })
+
+})
+
+
+
+
+
 
 app.listen(3002, () => {
     console.log("Server Running");
 })
+

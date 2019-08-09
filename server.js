17a5345bdd3b14e9964e4dadd04f55f09fdacdcc
kishan.si <kishan.si@media.net>
2019-08-09 19:11:40 +0530

    Frontend with websocket created

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
 const app = express();
+const expressWs = require('express-ws') (app);
 
-app.get('/:id', (req, res) => {
 
-    const id = req.params.id
-    res.status(200).send(id);
+
+app.ws('/', (ws, req) => {
+    
+    ws.on('message', (msg) => {
+        const text = JSON.parse(msg);
+        console.log(msg);
+    })   
 });
 
 app.listen(3002, () => {
     console.log("Server Running");
-})
+});

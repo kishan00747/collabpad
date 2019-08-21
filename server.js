91b74b0690fe5ed3664223b17a8aceb209157415
kishan.si <kishan.si@media.net>
2019-08-21 17:15:58 +0530

    Fixed process.env.port to PORT

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.port || 3002;
+const port = process.env.PORT || 3002;
 const redis = require('./redis.config');
 const fs = require('fs');
 const cors = require('cors');

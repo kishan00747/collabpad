3b312fc167a1d2079ce3e0f76ece7c9724d27ce7
kishan.si <kishan.si@media.net>
2019-09-03 17:48:37 +0530

    auto redirecting to https fix

--- server.js
+++ server.js
@@ -32,12 +32,12 @@ app.use(session({
   }
 }))
 
-app.use(function(req, res, next) {
-    if(!req.secure) {
-      return res.redirect(['https://', req.hostname, req.url].join(''));
-    }
-    next();
-  });
+// app.use(function(req, res, next) {
+//     if(!req.secure) {
+//       return res.redirect(['https://', req.hostname, req.url].join(''));
+//     }
+//     next();
+//   });
 
 app.use(bodyParser.json());
 app.use(morgan('combined', {stream: accessLogStream}));
